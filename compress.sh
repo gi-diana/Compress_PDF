50967449d34434e8c9f0c5eb52d5cf7c79922d55
Luigi Di Rado <luigi.dirado@gmail.com>
2020-04-26 11:15:33 +0200

    du file size

--- compress.sh
+++ compress.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 FILE="$1"
+du -h "$1"
 gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPrinted=false -dPDFSETTINGS=/prepress -dNOPAUSE -dQUIET -dBATCH -sOutputFile="$FILE"_small.pdf "$FILE"
 gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPrinted=false -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile="$FILE"_micro.pdf "$FILE"
-echo "done compress "$FILE"_small.pdf "$FILE"_micro.pdf"
+echo "done compress"  
+du -h "$FILE"_small.pdf 
+du -h  "$FILE"_micro.pdf
